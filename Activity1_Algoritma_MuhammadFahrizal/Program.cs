38232a132f9aae53f34e4a7b3946d9ea1b8690e8
muhfa <muhfa@LAPTOP-4AVLK6M6>
2022-10-04 07:58:55 +0700

    Add project files.

---
+++ Activity1_Algoritma_MuhammadFahrizal/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity1_Algoritma_MuhammadFahrizal
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
