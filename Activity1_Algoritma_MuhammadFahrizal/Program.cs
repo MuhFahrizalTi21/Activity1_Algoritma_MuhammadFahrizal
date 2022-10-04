c9f7830fae7e9ed043b44e50fa86918be8ebd1e5
Muhammad Fahrizal <m.fahrizal.ft21@mail.umy.ac.id>
2022-10-04 08:01:22 +0700

    Read

--- Activity1_Algoritma_MuhammadFahrizal/Program.cs
+++ Activity1_Algoritma_MuhammadFahrizal/Program.cs
@@ -6,8 +6,42 @@ using System.Threading.Tasks;
 
 namespace Activity1_Algoritma_MuhammadFahrizal
 {
+    class Program
+    {
+        // Deklarasi array int dengan ukuran 20
+        private int[] a = new int[20];
 
-    internal class Program
+        //Deklarasi variable int untuk menyimpan banyaknya data pada array
+        private int n;
+
+        // Fungsi / method untuk menerima masukan
+        public void read()
+        {
+            //Menerima angka untuk menentukan banyaknya data yang disimpan padqa array
+            while (true)
+            {
+                Console.Write("Masukan banyaknya elemen pada array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray dapat mempunyai maksimal 20 elemen.\n");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine(" Masukkan elemen array ");
+            Console.WriteLine("-----------------------");
+
+            //Pengguna memasukan elemen pada array
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("<" + (1 + 1) + ">");
+                string s1 = Console.ReadLine();
+                a[i] = Int32.Parse(s1);
+            }
+        }
+        internal class Program
     {
         static void Main(string[] args)
         {
